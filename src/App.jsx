1e30af14d6db7fa5910759754f0e539b9006bfc6
Eidamiino <adam.pala199@gmail.com>
2024-07-03 15:53:46 +0200

    category1 specific subcategories

--- src/App.jsx
+++ src/App.jsx
@@ -40,18 +40,18 @@ const category2=[
   },
   {
     categoryLevel: 2,
-    categoryFullCode: "produkty2.zivotni_pojisteni",
-    categoryTitle: "Životní pojištění",
+    categoryFullCode: "produkty.zivotni_pojisteni2",
+    categoryTitle: "Životní pojištění2",
     categoryTooltip: "PRODUKTY➤Životní pojištění",
-    categoryIcon: "fas fa-heart",
+    categoryIcon: "fas fa-umbrella",
     scopes: null
   },
   {
     categoryLevel: 2,
     categoryFullCode: "produkty3.zivotni_pojisteni",
-    categoryTitle: "Životní pojištění",
+    categoryTitle: "Životní pojištění3",
     categoryTooltip: "PRODUKTY➤Životní pojištění",
-    categoryIcon: "fas fa-heart",
+    categoryIcon: "fas fa-home",
     scopes: null
   }
 ]
@@ -75,6 +75,12 @@ function App() {
     window.addEventListener('urlChangeCategory1', changeUrl);
   });
 
+  const getFilteredSubcategories = () => {
+    if (!categoryPath()) return [];
+    console.log("snazim" + categoryPath())
+    return category2.filter(subcategory => subcategory.categoryFullCode.split(".")[0]==categoryPath().split(".")[0]);
+  };
+
   return (
     <div>
       <div class='categories level-1' style="display:flex; justify-content:center;">
@@ -86,8 +92,7 @@ function App() {
         <div class='row'>
           {categoryPath() && (
             <div>
-              <h2>hroch: {categoryPath()}</h2>
-              <Category2Group items={category2} />
+              <Category2Group items={getFilteredSubcategories()} />
             </div>
           )}
         </div>
